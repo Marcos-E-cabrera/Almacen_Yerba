b3106b7088dfbea9bd6230fd39906c386f543a46
marcoscabrera <marcoscabrera021@gmail.com>
2023-09-12 10:11:04 -0300

    Creacion del proyecto

---
+++ CapaEntidad/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Cliente
+    {
+        public int IdNombre { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public bool Reestablecer { get; set; }
+    }
+}
